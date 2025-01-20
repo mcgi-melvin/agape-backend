6f4d3fcda6084d74fdd3001f0fc4b180e8cd837c
mcgi-melvin <melvinlomibao@gmail.com>
2025-01-20 14:10:36 +0800

    Initial Commit

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ require('dotenv').config()
 
 const
     express = require('express'),
-    app = express(),
-    port = process.env.APP_PORT
+    app = express()
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
+app.listen(process.env.APP_PORT, () => {
+    console.log(`App listening on port ${process.env.APP_PORT}`)
 })
