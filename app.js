fb8e8d780a7cbbec8b7ebae6e355123a4c7532ad
mcgi-melvin <melvinlomibao@gmail.com>
2025-01-20 14:21:05 +0800

    Initial Commit

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const
     express = require('express'),
     app = express()
 
+app.get('/', (req, res) => {
+    res.send('Testing')
+})
 app.listen(process.env.APP_PORT, () => {
     console.log(`App listening on port ${process.env.APP_PORT}`)
 })
